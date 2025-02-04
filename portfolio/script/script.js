4db4af977bd20049ad8b1c8550a01c87daca2804
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-04 20:36:29 -0300

    Atualizacao 2.0

--- portfolio/script/script.js
+++ portfolio/script/script.js
@@ -25,3 +25,24 @@ const observer = new IntersectionObserver(entries => {
 sections.forEach(section => {
     observer.observe(section);
 });
+
+// Função para formatar o telefone
+function formatarTelefone(event) {
+    // Remove tudo o que não for número
+    let telefone = event.target.value.replace(/\D/g, '');
+  
+    // Formata o telefone conforme o padrão (XX) XXXX-XXXXX
+    if (telefone.length <= 2) {
+      telefone = telefone.replace(/(\d{2})/, '($1)');
+    } else if (telefone.length <= 6) {
+      telefone = telefone.replace(/(\d{2})(\d{4})/, '($1) $2');
+    } else {
+      telefone = telefone.replace(/(\d{2})(\d{4})(\d{5})/, '($1) $2-$3');
+    }
+  
+    // Atualiza o valor do input com o telefone formatado
+    event.target.value = telefone;
+  }
+  
+  // Adiciona o evento de digitação ao input
+  document.getElementById('telefone').addEventListener('input', formatarTelefone);  
